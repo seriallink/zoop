76224098afb819d8d2490ebbb4c3f3647557c47e
Marcelo Monaco <marcelo.monaco@gmail.com>
2019-01-31 12:33:42 -0200

    Added Reasons

--- error.go
+++ error.go
@@ -2,11 +2,6 @@ package zoop
 
 type ErrImpl interface {
 	Error() string
-	GetStatus() string
-	GetStatusCode() int
-	GetType() string
-	GetCategory() string
-	GetMessage() string
 }
 
 type ErrResponse struct {
@@ -14,33 +9,14 @@ type ErrResponse struct {
 }
 
 type ErrObject struct {
-	Status     string `json:"status"`
-	StatusCode int    `json:"status_code"`
-	Type       string `json:"type"`
-	Category   string `json:"category"`
-	Message    string `json:"message"`
+	Status     string   `json:"status"`
+	StatusCode int      `json:"status_code"`
+	Type       string   `json:"type"`
+	Category   string   `json:"category"`
+	Message    string   `json:"message"`
+	Reasons    []string `json:"reasons"`
 }
 
 func (err ErrObject) Error() string {
-	return err.Message
-}
-
-func (err ErrObject) GetStatus() string {
-	return err.Status
-}
-
-func (err ErrObject) GetStatusCode() int {
-	return err.StatusCode
-}
-
-func (err ErrObject) GetType() string {
-	return err.Type
-}
-
-func (err ErrObject) GetCategory() string {
-	return err.Category
-}
-
-func (err ErrObject) GetMessage() string {
 	return err.Message
 }
